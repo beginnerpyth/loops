e2e76eae09c90bfe5332c0408389104945ae1974
beginnerpyth <hskx7779@gmail.com>
2025-12-28 17:49:05 +0900

    Add files via upload
    beginneer loop

---
+++ for loop.py
@@ -0,0 +1,12 @@
+#friends=["jim","mama","papa"]
+#len=len(friends)
+#for letter in range(len):
+ #   print(letter)
+g = [
+    (1,2,3),
+    (4,5,6),
+    (7,8,9)
+     ]
+for row in g:
+    for column in row:
+        print(column)
